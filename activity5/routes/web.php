67b7cb7fdb11068bc9e62a3f8a165a87c2f0d3d8
Clarizze <clarizzesaisay@gmail.com>
2023-11-25 22:49:05 +0800

    activity5

--- activity5/routes/web.php
+++ activity5/routes/web.php
@@ -1,32 +1,22 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\SignInController;
+use App\Http\Controllers\SignUpController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/register', function () {
-    return view('register');
-})->name('register');
+    return view('Auth.login');
+})->name('Auth.login');
 
-Route::post('/register_submit', 'App\Http\Controllers\RegisterController@register_submit')
-    ->name('register_submit');
+Route::get('/signup', function () {
+    return view('Auth.signup');
+})->name('signup');
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
+Route::post('/create', [SignUpController::class, 'create'])->name('register');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::get('/login', [SignInController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [SignInController::class, 'login']);
+Route::post('/logout', [SignInController::class, 'logout'])->name('logout');
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
